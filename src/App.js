f4b8e602e963269b8c327c1ba76874b1715844b1
Bao <quocbaole5420@gmail.com>
2021-11-16 18:37:42 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes
+} from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,65 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Marketing Agency',
+  start: '',
+  img: require('./../../img/svg-1.svg').default,
+  alt: 'Image'
+
+}
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Marketing Agency',
+  start: '',
+  img: require('./../../img/profile.jpg').default,
+  alt: 'Image'
+
+}
+export const homeObjThree = {
+  lightBg: false,
+  imgStart: 'start',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Marketing Agency',
+  primary: true,
+  start: '',
+  img: require('./../../img/svg-3.svg').default,
+  alt: 'Image'
+
+}
+export const homeObjFour = {
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Marketing Agency',
+  primary: true,
+  start: '',
+  img: require('./../../img/svg-2.svg').default,
+  alt: 'Image'
+
+}
